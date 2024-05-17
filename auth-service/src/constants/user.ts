2becc1abada934e1e3d9eb1fb8f9514c238a3827
Ihar <igor.tsykala@envisionblockchain.com>
2024-05-17 18:59:56 +0500

    fix: issue-3447-redundant-fields-in-session-api

---
+++ auth-service/src/constants/user.ts
@@ -0,0 +1,16 @@
+export const REQUIRED_PROPS = {
+  STATUS_CODE: 'status_code',
+  REASON: 'reason',
+  USER_NAME: 'username',
+  DID: 'did',
+  PARENT: 'parent',
+  HEDERA_ACCOUNT_ID: 'hederaAccountId',
+  ROLE: 'role',
+  ID: 'id',
+
+  // _ID: '_id',
+  // CREATE_DATE: 'createdDate',
+  // UPDATE_DATE: 'updatedDate',
+  // WALLET_TOKEN: 'walletToken',
+  // REFRESH_TOKEN: 'refreshToken',
+};
